737b007628af7e3e371e3b78576164779db134a4
MarcusSarmento <mvddssarmento@hotmail.com>
2025-10-15 22:03:15 -0300

    feat: Implementando  Tag BrowserRouter

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/navbar/NavBar.jsx"
+
+export default function App() {
+  return (
+    <>
+          <header>
+              <Navbar />
+          </header>
+          <main></main>
+          <footer></footer>
+
+          {/* <Login/> */}
+          {/* <Cadastro/> */}
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import Login from './pages/Login/Login.jsx'
+import App from './App'
 import './global.css'
-//import Cadastro from './pages/Cadastro/Cadastro.jsx'
-import Navbar from './components/navbar/NavBar.jsx'
+
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <header>
-      <Navbar/>
-    </header>
-    <main></main>
-    <footer></footer>
-    
-    {/* <Login/> */}
-    {/* <Cadastro/> */}
+    <App/>
   </StrictMode>,
 )
