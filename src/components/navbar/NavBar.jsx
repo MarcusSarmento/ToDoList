25f521b0409e0e1a594d513f1e31c0c850205740
MarcusSarmento <mvddssarmento@hotmail.com>
2025-10-15 21:21:05 -0300

    feat: configurar navbar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>ToDo</h1>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/tarefas">Tarefas</a></li>
+        </ul>
+    </nav>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import Login from './pages/Login/Login.jsx'
-import './pages/global.css'
-import Cadastro from './pages/Cadastro/Cadastro.jsx'
+import './global.css'
+//import Cadastro from './pages/Cadastro/Cadastro.jsx'
+import Navbar from './components/navbar/NavBar.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <header>
+      <Navbar/>
+    </header>
+    <main></main>
+    <footer></footer>
+    
     {/* <Login/> */}
-    <Cadastro/>
+    {/* <Cadastro/> */}
   </StrictMode>,
 )
